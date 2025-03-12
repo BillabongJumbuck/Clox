022cebe6335bd3328ee8e70b514e2cd5b92dc0c5
piglet <2792904471@qq.com>
2025-03-12 16:15:46 +0800

    Closure

--- compiler.c
+++ compiler.c
@@ -46,7 +46,13 @@ typedef struct {
 typedef struct {
     Token name;
     int depth;
+    bool isCaptured;
 } Local;
+
+typedef struct {
+    uint8_t index;
+    bool isLocal;
+} Upvalue;
 
 typedef enum {
     TYPE_FUNCTION,
@@ -59,6 +65,7 @@ typedef struct Compiler{
     FunctionType type;
     Local locals[UINT8_COUNT];
     int localCount;
+    Upvalue upvalues[UINT8_COUNT]; // 函数的捕获的被闭包变量的列表
     int scopeDepth;
 } Compiler;
 
@@ -235,6 +242,7 @@ static void initCompiler(Compiler* compiler, FunctionType type) {
     
     Local* local = &current->locals[current->localCount++];
     local->depth = 0;
+    local->isCaptured = false;
     local->name.start = "";
     local->name.length = 0;
 }
@@ -424,7 +432,11 @@ static void endScope() {
 
     while (current->localCount > 0 &&
             current->locals[current->localCount-1].depth > current->scopeDepth) {
-        emitByte(OP_POP);
+        if (current->locals[current->localCount - 1].isCaptured) {
+            emitByte(OP_CLOSE_UPVALUE);
+        } else {
+            emitByte(OP_POP);
+        }
         current->localCount--;
     }
 }
@@ -598,6 +610,7 @@ static void addLocal(Token name) {
     Local* local = &current->locals[current->localCount++];
     local->name = name;
     local->depth = -1; // 表示该变量已“声明”但未“初始化”。
+    local->isCaptured = false;
 }
 
 /**
@@ -700,13 +713,70 @@ static int resolveLocal(Compiler* compiler, Token* name) {
     return -1;
 }
 
+/**
+ * 将捕获到的被closed的局部变量加到函数的upvalue数组中
+ * @param compiler 当前的“编译器”
+ * @param index 被捕获到的局部变量在
+ * @param isLocal
+ * @return 局部变量在upvalues数组中的索引 0->数组已满
+ */
+static int addUpvalue(Compiler* compiler, uint8_t index, bool isLocal) {
+    int upvalueCount = compiler->function->upvalueCount;
+
+    // Check duplication
+    for (int i = 0;i < upvalueCount; i++) {
+        Upvalue* upvalue = &compiler->upvalues[i];
+        if (upvalue->index == index && upvalue->isLocal == isLocal) {
+            return i;
+        }
+    }
+
+    if (upvalueCount == UINT8_COUNT) {
+        error("Too many closure variables in function.");
+        return 0;
+    }
+
+    compiler->upvalues[upvalueCount].isLocal = isLocal;
+    compiler->upvalues[upvalueCount].index = index;
+    return compiler->function->upvalueCount++;
+}
+
+/**
+ * 在Upvalues array中寻找被闭包的变量
+ * @param compiler 当前的Compiler
+ * @param name 变量名称
+ * @return if -1 failure else upvalue index.
+ */
+static int resolveUpvalue(Compiler* compiler, Token* name) {
+    if (compiler->enclosing == NULL) return -1;  // Top Level
+
+    // 从上一层函数中寻找待捕获的局部变量
+    int local = resolveLocal(compiler->enclosing, name);
+    if (local != -1) {
+        compiler->enclosing->locals[local].isCaptured = true;
+        return addUpvalue(compiler, (uint8_t)local, true);
+    }
+
+    // 从上一层函数的闭包中寻找待捕获的局部变量
+    int upvalue = resolveUpvalue(compiler->enclosing, name);
+    if (upvalue != -1) {
+        return addUpvalue(compiler, (uint8_t)upvalue, false);
+    }
+
+    return -1;
+}
+
 static void namedVariable(Token name, bool canAssign) {
     uint8_t getOp, setOp;
     int arg = resolveLocal(current, &name);
     if (arg != -1) {
         getOp = OP_GET_LOCAL;
         setOp = OP_SET_LOCAL;
-    } else {
+    } else if ((arg = resolveUpvalue(current, &name)) != -1) {
+        getOp = OP_GET_UPVALUE;
+        setOp = OP_SET_UPVALUE;
+    }
+    else {
         arg = identifierConstant(&name);
         getOp = OP_GET_GLOBAL;
         setOp = OP_SET_GLOBAL;
@@ -745,7 +815,12 @@ static void function(FunctionType type) {
     block();
 
     ObjFunction* function = endCompiler();
-    emitBytes(OP_CONSTANT, makeConstant(OBJ_VAL(function)));
+    emitBytes(OP_CLOSURE, makeConstant(OBJ_VAL(function)));
+
+    for (int i=0;i<function->upvalueCount;i++) {
+        emitByte(compiler.upvalues[i].isLocal ? 1 : 0);
+        emitByte(compiler.upvalues[i].index);
+    }
 }
 
 static void funDeclaration() {
