7bb9e285fd1e6ac9c0e96234ebfcdf5b681145c0
piglet <2792904471@qq.com>
2025-03-16 15:43:10 +0800

    Superclasses

--- vm.c
+++ vm.c
@@ -210,7 +210,7 @@ static bool bindMethod(ObjClass *klass, ObjString *name) {
     return true;
 }
 
-static bool invokeFromClass(ObjClass* klass, ObjString* name, int argCount) {
+static bool invokeFromClass(ObjClass *klass, ObjString *name, int argCount) {
     Value method;
     if (!tableGet(&klass->methods, name, &method)) {
         runtimeError("Undefined property '%s'.", name->chars);
@@ -219,7 +219,7 @@ static bool invokeFromClass(ObjClass* klass, ObjString* name, int argCount) {
     return call(AS_CLOSURE(method), argCount);
 }
 
-static bool invoke(ObjString* name, int argCount) {
+static bool invoke(ObjString *name, int argCount) {
     Value receiver = peek(argCount);
 
     if (!IS_INSTANCE(receiver)) {
@@ -227,7 +227,7 @@ static bool invoke(ObjString* name, int argCount) {
         return false;
     }
 
-    ObjInstance* instance = AS_INSTANCE(receiver);
+    ObjInstance *instance = AS_INSTANCE(receiver);
 
     Value value;
     if (tableGet(&instance->fields, name, &value)) {
@@ -397,6 +397,15 @@ static InterpretResult run() {
                 push(value);
                 break;
             }
+            case OP_GET_SUPER: {
+                ObjString *name = READ_STRING();
+                ObjClass *superclass = AS_CLASS(pop());
+
+                if (!bindMethod(superclass, name)) {
+                    return INTERPRET_RUNTIME_ERROR;
+                }
+                break;
+            }
             case OP_EQUAL : {
                 Value b = pop();
                 Value a = pop();
@@ -470,12 +479,22 @@ static InterpretResult run() {
                 break;
             }
             case OP_INVOKE: {
-                ObjString* method = READ_STRING();
+                ObjString *method = READ_STRING();
                 int argCount = READ_BYTE();
                 if (!invoke(method, argCount)) {
                     return INTERPRET_RUNTIME_ERROR;
                 }
                 frame = &vm.frames[vm.frameCount - 1];
+                break;
+            }
+            case OP_SUPER_INVOKE: {
+                ObjString* method = READ_STRING();
+                int argCount = READ_BYTE();
+                ObjClass *superclass = AS_CLASS(pop());
+                if (!invokeFromClass(superclass, method, argCount)) {
+                    return INTERPRET_RUNTIME_ERROR;
+                }
+                frame = &vm.frames[vm.frameCount-1];
                 break;
             }
             case OP_CLOSURE: {
@@ -514,6 +533,18 @@ static InterpretResult run() {
             }
             case OP_CLASS: {
                 push(OBJ_VAL(newClass(READ_STRING())));
+                break;
+            }
+            case OP_INHERIT: {
+                Value superclass = peek(1);
+                if (!IS_CLASS(superclass)) {
+                    runtimeError("Superclass must be a class.");
+                    return INTERPRET_RUNTIME_ERROR;
+                }
+
+                ObjClass *subclass = AS_CLASS(peek(0));
+                tableAddAll(&AS_CLASS(superclass)->methods, &subclass->methods);
+                pop(); // Subclass
                 break;
             }
             case OP_METHOD: {
